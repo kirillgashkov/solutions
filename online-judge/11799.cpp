36f6fac87bd35b6ed8a13df0b9b893a4fe1f2d81
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-08-01 17:30:54 +0500

    Solve some easy problems

---
+++ online-judge/11799.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 1; i <= T; ++i) {
+        int N;
+        cin >> N;
+        int max_s = -1;
+        while (N) {
+            --N;
+            int s;
+            cin >> s;
+            if (s > max_s) {
+                max_s = s;
+            }
+        }
+        cout << "Case " << i << ": " << max_s << '\n';
+    }
+}
