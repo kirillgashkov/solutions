ca9e9f25c0e133fef0d911bd1c08c8107cd15621
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-11-10 19:08:59 +0500

    Solve 'ID Codes'

---
+++ online-judge/00146.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    while (true) {
+        string code;
+        cin >> code;
+
+        if (code == "#") break;
+
+
+        string sorted_code(code);
+        sort(sorted_code.begin(), sorted_code.end());
+
+        string next_code(code);
+        next_permutation(next_code.begin(), next_code.end());
+
+
+        if (sorted_code == next_code) {
+            cout << "No Successor\n";
+        } else {
+            cout << next_code << '\n';
+        }
+    }
+}
