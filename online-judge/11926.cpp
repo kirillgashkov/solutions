3d2b4216703cc32d2e18f7b0472ef67e07ebeb9c
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-11-15 00:12:57 +0500

    Solve 'Multitasking'

---
+++ online-judge/11926.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+
+int main() {
+    while (true) {
+        int n, m;
+        cin >> n >> m;
+
+        if (n == 0 and m == 0) break;
+
+
+        bitset<1000000> calendar;
+        bool has_conflict = false;
+
+
+        for (int i = 0; i < n; ++i) {
+            int a, b;
+            cin >> a >> b;
+
+            for (int j = a; j < b; ++j) {
+                if (calendar[j]) {
+                    has_conflict = true;
+                }
+                calendar[j] = true;
+            }
+        }
+
+
+        for (int i = 0; i < m; ++i) {
+            int a, b, s;
+            cin >> a >> b >> s;
+
+            while (a < 1000000) {
+                if (b > 1000000) b = 1000000;
+
+                for (int j = a; j < b; ++j) {
+                    if (calendar[j]) {
+                        has_conflict = true;
+                    }
+                    calendar[j] = true;
+                }
+
+                a += s;
+                b += s;
+            }
+        }
+
+
+        if (has_conflict) {
+            cout << "CONFLICT\n";
+        } else {
+            cout << "NO CONFLICT\n";
+        }
+    }
+}
