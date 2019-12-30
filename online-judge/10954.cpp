c25cb7a837590eb8ebf44205f0bc967803b5969c
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-12-30 14:07:47 +0500

    Solve 'Add All'

---
+++ online-judge/10954.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+using namespace std;
+
+#define intl unsigned long long
+
+
+int main() {
+    while (true) {
+        int n;
+        cin >> n;
+
+        if (n == 0) {
+            break;
+        }
+
+
+        priority_queue<int, vector<int>, greater<int>> nums;
+        intl target_sum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int num;
+            cin >> num;
+            nums.push(num);
+            target_sum += num;
+        }
+
+
+        intl cost = 0;
+
+        while (true) {
+            int sum = nums.top();
+            nums.pop();
+            sum += nums.top();
+            nums.pop();
+
+            cost += sum;
+
+            if (sum == target_sum) {
+                break;
+            }
+
+            nums.push(sum);
+        }
+
+
+        cout << cost << '\n';
+    }
+}
